ad6b5a5f5b1b5b9e08803c024bbb96ac0169a096
Nicholas Addison <>
2019-05-08 17:20:08 +1000

    Added build contracts and config

--- scratch/extensions/config.js
+++ scratch/extensions/config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    infuraProjectId: "your Infura Project Id",
-    privateKeys: ["your private key"],
+    infuraProjectId: "6c9fdf9ed3a64dd4b3519ee1609493e4",
+    privateKeys: ["29993AE0B3357C40C735BB5BC38652972FBC8CA4F5EBF6249815C358737FF711"],
+    // infuraProjectId: "your Infura Project Id",
+    // privateKeys: ["your private key"],
 }
